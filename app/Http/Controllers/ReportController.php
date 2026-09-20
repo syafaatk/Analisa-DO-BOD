91c63517a01c7def30dadace8ae6eb935b56e7e4
syafaatk <syafaatkhoirusy@gmail.com>
2026-09-21 00:45:22 +0700

    Render printable laboratory report view

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,5 +4,5 @@ use App\Services\ReportDataService;
 class ReportController extends Controller
 {
  public function __construct(private ReportDataService $service){}
- public function show(int $id){return response()->json($this->service->result($id));}
+ public function show(int $id){return view('reports.analysis',['report'=>$this->service->result($id)]);}
 }
