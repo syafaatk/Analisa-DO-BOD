ec9d52962b0b5122489e3e08f36746463dcdbc49
syafaatk <syafaatkhoirusy@gmail.com>
2026-09-21 00:55:55 +0700

    Use UUID model identifiers

--- app/Models/UncertaintyComponent.php
+++ app/Models/UncertaintyComponent.php
@@ -1,8 +1,10 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use App\Models\Concerns\BelongsToLaboratory;
 class UncertaintyComponent extends Model {
+ use HasUuids;
  use BelongsToLaboratory;
  protected $fillable=['laboratory_id','uncertainty_model_id','code','name','source_type','distribution','standard_uncertainty','sensitivity_coefficient','contribution','degrees_of_freedom','relative_contribution_percent','observations'];
  protected $casts=['observations'=>'array'];
