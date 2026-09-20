c820c2943dc66f0c3577b3dc263b518e1ef6a6f5
syafaatk <syafaatkhoirusy@gmail.com>
2026-09-21 01:21:58 +0700

    feat: register client auth middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,3 @@
 <?php
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
-return Application::configure(basePath: dirname(__DIR__))
- ->withRouting(web: __DIR__.'/../routes/web.php',commands: __DIR__.'/../routes/console.php',health:'/up')
- ->withMiddleware(function(Middleware $middleware){$middleware->alias(['lab.auth'=>\App\Http\Middleware\LabAuth::class,'role'=>\App\Http\Middleware\RoleMiddleware::class,'lab.context'=>\App\Http\Middleware\LaboratoryContext::class]);})
- ->withExceptions(function(Exceptions $exceptions){})
- ->create();
+use Illuminate\Foundation\Application;use Illuminate\Foundation\Configuration\Exceptions;use Illuminate\Foundation\Configuration\Middleware;
+return Application::configure(basePath: dirname(__DIR__))->withRouting(web: __DIR__.'/../routes/web.php',commands: __DIR__.'/../routes/console.php',health:'/up')->withMiddleware(function(Middleware $middleware){$middleware->alias(['lab.auth'=>\App\Http\Middleware\LabAuth::class,'client.auth'=>\App\Http\Middleware\ClientAuth::class,'role'=>\App\Http\Middleware\RoleMiddleware::class,'lab.context'=>\App\Http\Middleware\LaboratoryContext::class]);})->withExceptions(function(Exceptions $exceptions){})->create();
