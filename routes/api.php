1fbe1537e36c4ece70962f27fcb84a693f27b15e
syafaatk <syafaatkhoirusy@gmail.com>
2026-09-20 22:26:39 +0700

    feat: expose analysis report API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UncertaintyController;
+use App\Http\Controllers\ReportController;
 Route::post('/uncertainty/calculate',[UncertaintyController::class,'calculate']);
 Route::post('/uncertainty/statistics',[UncertaintyController::class,'statistics']);
+Route::get('/analysis/{id}/report',[ReportController::class,'show']);
