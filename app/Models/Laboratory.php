93e61b794346d42005989d3423a94188f659a1d4
syafaatk <syafaatkhoirusy@gmail.com>
2026-09-21 00:55:39 +0700

    Use UUID model identifiers

--- app/Models/Laboratory.php
+++ app/Models/Laboratory.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 class Laboratory extends Model {
+ use HasUuids;
  protected $fillable=['code','name','active','address','phone','email'];
  protected $casts=['active'=>'boolean'];
  public function users(){return $this->hasMany(LabUser::class,'laboratory_id');}
