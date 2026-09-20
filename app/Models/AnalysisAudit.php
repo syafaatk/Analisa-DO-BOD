16787a627a9991bd13e7e815957843e1fc3b0ac2
syafaatk <syafaatkhoirusy@gmail.com>
2026-09-21 00:49:09 +0700

    Scope model data by laboratory

--- app/Models/AnalysisAudit.php
+++ app/Models/AnalysisAudit.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Concerns\BelongsToLaboratory;
 class AnalysisAudit extends Model {
- protected $fillable=['analysis_run_id','actor','action','from_status','to_status','changes','notes'];
+ use BelongsToLaboratory;
+ protected $fillable=['laboratory_id','analysis_run_id','actor','action','from_status','to_status','changes','notes'];
  protected $casts=['changes'=>'array'];
 }
