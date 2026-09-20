5165ce9a72488757bb3cf1f84a86e864ab387126
syafaatk <syafaatkhoirusy@gmail.com>
2026-09-21 00:49:06 +0700

    Scope model data by laboratory

--- app/Models/BodControl.php
+++ app/Models/BodControl.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Concerns\BelongsToLaboratory;
 class BodControl extends Model {
- protected $fillable=['analysis_run_id','control_type','control_code','do_initial','do_final','bod_result','expected_min','expected_max','acceptance_percent','status','notes'];
+ use BelongsToLaboratory;
+ protected $fillable=['laboratory_id','analysis_run_id','control_type','control_code','do_initial','do_final','bod_result','expected_min','expected_max','acceptance_percent','status','notes'];
  protected $casts=['do_initial'=>'float','do_final'=>'float','bod_result'=>'float','expected_min'=>'float','expected_max'=>'float','acceptance_percent'=>'float'];
 }
