0a4cda59eb0a7e2df6d69d035c623e830964909e
syafaatk <syafaatkhoirusy@gmail.com>
2026-09-20 22:28:17 +0700

    feat: add sample management routes

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,6 @@ use App\Http\Controllers\ReportController;
 Route::post('/uncertainty/calculate',[UncertaintyController::class,'calculate']);
 Route::post('/uncertainty/statistics',[UncertaintyController::class,'statistics']);
 Route::get('/analysis/{id}/report',[ReportController::class,'show']);
+
+use App\Http\Controllers\SampleController;
+Route::apiResource('/samples',SampleController::class)->only(['index','store','show']);
