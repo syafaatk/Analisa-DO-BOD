b822957f4ae43d7083dc0c5a339ad93988be10e5
syafaatk <syafaatkhoirusy@gmail.com>
2026-09-21 00:48:52 +0700

    Scope model data by laboratory

--- app/Models/Reagent.php
+++ app/Models/Reagent.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Concerns\BelongsToLaboratory;
 class Reagent extends Model {
- protected $fillable=['code','name','lot_number','concentration','unit','expiry_date'];
+ use BelongsToLaboratory;
+ protected $fillable=['laboratory_id','code','name','lot_number','concentration','unit','expiry_date'];
  protected $casts=['concentration'=>'decimal:8','expiry_date'=>'date'];
 }
