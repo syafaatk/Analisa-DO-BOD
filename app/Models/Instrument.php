a7321de0490507706f9c2511bf4d6ea3c3091d6f
syafaatk <syafaatkhoirusy@gmail.com>
2026-09-21 00:48:56 +0700

    Scope model data by laboratory

--- app/Models/Instrument.php
+++ app/Models/Instrument.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Concerns\BelongsToLaboratory;
 class Instrument extends Model {
- protected $fillable=['code','name','serial_number','calibration_due','status'];
+ use BelongsToLaboratory;
+ protected $fillable=['laboratory_id','code','name','serial_number','calibration_due','status'];
  protected $casts=['calibration_due'=>'date'];
 }
