294331dc1fc74e1a4e735b1f4e74e38a37e4e3b6
syafaatk <syafaatkhoirusy@gmail.com>
2026-09-21 00:37:51 +0700

    Allow workflow and metadata fields on analysis records

--- app/Models/AnalysisRun.php
+++ app/Models/AnalysisRun.php
@@ -3,6 +3,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 class AnalysisRun extends Model
 {
- protected $fillable=['sample_code','parameter','method_version','analyst','analysed_at','inputs','calculation','uncertainty','notes'];
- protected $casts=['inputs'=>'array','calculation'=>'array','uncertainty'=>'array','analysed_at'=>'datetime'];
+ protected $fillable=['sample_code','parameter','method_version','analyst','analysed_at','inputs','calculation','uncertainty','notes','status','approved_by','approved_at','sample_matrix','batch_code','instrument_code','reagent_lot','report_number','submitted_at'];
+ protected $casts=['inputs'=>'array','calculation'=>'array','uncertainty'=>'array','analysed_at'=>'datetime','approved_at'=>'datetime','submitted_at'=>'datetime'];
 }
