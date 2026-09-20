2479a2ea3fc6e4ef96971a52b850eee8af3c653e
syafaatk <syafaatkhoirusy@gmail.com>
2026-09-21 00:49:12 +0700

    Scope model data by laboratory

--- app/Models/AnalysisReview.php
+++ app/Models/AnalysisReview.php
@@ -1,9 +1,11 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Concerns\BelongsToLaboratory;
 class AnalysisReview extends Model
 {
- protected $fillable=['analysis_run_id','reviewer','decision','comments','reviewed_at'];
+ use BelongsToLaboratory;
+ protected $fillable=['laboratory_id','analysis_run_id','reviewer','decision','comments','reviewed_at'];
  protected $casts=['reviewed_at'=>'datetime'];
  public function analysis(){return $this->belongsTo(AnalysisRun::class,'analysis_run_id');}
 }
