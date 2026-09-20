89695f333112d969eb9d4d505a9c3709a4fb51b3
syafaatk <syafaatkhoirusy@gmail.com>
2026-09-21 00:42:55 +0700

    Add BOD and audit relationships

--- app/Models/AnalysisRun.php
+++ app/Models/AnalysisRun.php
@@ -3,6 +3,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 class AnalysisRun extends Model
 {
+ public function bodDilutions(){return $this->hasMany(BodDilution::class,'analysis_run_id');}
+ public function bodControls(){return $this->hasMany(BodControl::class,'analysis_run_id');}
+ public function audits(){return $this->hasMany(AnalysisAudit::class,'analysis_run_id');}
  protected $fillable=['sample_code','parameter','method_version','analyst','analysed_at','inputs','calculation','uncertainty','notes','status','approved_by','approved_at','sample_matrix','batch_code','instrument_code','reagent_lot','report_number','submitted_at'];
  protected $casts=['inputs'=>'array','calculation'=>'array','uncertainty'=>'array','analysed_at'=>'datetime','approved_at'=>'datetime','submitted_at'=>'datetime'];
 }
