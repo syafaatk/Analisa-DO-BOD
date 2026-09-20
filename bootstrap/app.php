36681a3188e57d32147e0e9c5af3e5de0954315e
syafaatk <syafaatkhoirusy@gmail.com>
2026-09-21 00:16:20 +0700

    Register laboratory auth middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,6 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
  ->withRouting(web: __DIR__.'/../routes/web.php',commands: __DIR__.'/../routes/console.php',health:'/up')
- ->withMiddleware(function(Middleware $middleware){})
+ ->withMiddleware(function(Middleware $middleware){$middleware->alias(['lab.auth'=>\App\Http\Middleware\LabAuth::class,'role'=>\App\Http\Middleware\RoleMiddleware::class]);})
  ->withExceptions(function(Exceptions $exceptions){})
  ->create();
